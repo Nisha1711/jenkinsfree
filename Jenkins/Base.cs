cadd20ca49daeb021ec3c084e132faa65eaa939e
nishathangaraj <nishathangaraj@outlook.com>
2018-08-13 12:26:06 +0530

    Add project files.

---
+++ Jenkins/Base.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+namespace Jenkins
+{
+    public class Base
+    {
+
+        public IWebDriver Driver { get; set; }
+
+    }
+}
---
+++ Jenkins/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Jenkins
+{
+    [TestFixture]
+    public class UnitTest1:Hooks
+    {
+
+
+
+        [Test]
+        public void Test1()
+        {
+
+            Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            Driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            Driver.FindElement(By.Name("Password")).SendKeys("admin");
+            Driver.FindElement(By.Name("Login")).Click();
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            SelectElement oSelect = new SelectElement(Driver.FindElement(By.Id("TitleId")));
+            oSelect.SelectByIndex(1);
+            Driver.FindElement(By.Id("FirstName")).SendKeys("Nisha");
+
+        }
+    }
+}
